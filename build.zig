30a715ab1030375b96d87fcd750921a14dc1736b
protim <mail@protimroy.com>
2026-04-10 14:54:04 -0400

    Publish CNAME with site build

--- build.zig
+++ build.zig
@@ -4,24 +4,27 @@ const Goku = @import("goku").Goku;
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    
+
     const site_path = b.path("");
     const out_path = b.path("build");
-    
+    const static_path = b.path("static");
+
     const goku_dep = b.dependency("goku", .{
         .target = target,
         .optimize = optimize,
     });
-    
+
     const site_step = b.step("site", "Build the site with Goku");
-    
+
     // Use the Goku build method
     const build_site = Goku.build(goku_dep, site_path, out_path);
-    
-    site_step.dependOn(&build_site.step);
-    
+    const copy_static = Goku.copyStatic(goku_dep, static_path, out_path);
+    copy_static.step.dependOn(&build_site.step);
+
+    site_step.dependOn(&copy_static.step);
+
     const serve_step = b.step("serve", "Serve the built Goku site");
     const serve_site = Goku.preview(goku_dep, site_path, out_path);
-    serve_site.step.dependOn(&build_site.step);
+    serve_site.step.dependOn(&copy_static.step);
     serve_step.dependOn(&serve_site.step);
 }
